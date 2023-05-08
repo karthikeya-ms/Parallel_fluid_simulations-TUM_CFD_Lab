d0c314ac342e3f3a1a12ab54144896d5cce69995
Harikrishnan Anathazhath <ge83rat@mytum.de>
2023-05-08 19:14:36 +0200

    Update src/Boundary.cpp

--- src/Boundary.cpp
+++ src/Boundary.cpp
@@ -7,50 +7,42 @@ FixedWallBoundary::FixedWallBoundary(std::vector<Cell *> cells) : _cells(cells) 
 FixedWallBoundary::FixedWallBoundary(std::vector<Cell *> cells, std::map<int, double> wall_temperature)
     : _cells(cells), _wall_temperature(wall_temperature) {}
 
+//Added: Function implemented for first task.
 void FixedWallBoundary::apply(Fields &field) {
-    int i_idx{0};
+
+	int i_idx{0};
 	int j_idx{0};
 
 	for (const auto cell : _cells){
 	
 		i_idx = cell->i();
 		j_idx = cell->j();
-        bool tw = cell->is_border(border_position::TOP);
-        bool rw = cell->is_border(border_position::RIGHT); 
-        bool bw = cell->is_border(border_position::BOTTOM);
-        bool lw = cell->is_border(border_position::LEFT);
-        bool con = true;
-
-        switch(con){
-            case tw:
-                field.p(i_idx, j_idx) = field.p(i_idx, j_idx + 1);
-			    field.u(i_idx, j_idx) = -field.u(i_idx, j_idx + 1);
-			    field.v(i_idx, j_idx) = 0; 
-			    field.g(i_idx, j_idx) = field.v(i_idx, j_idx);
-                break;
-            case rw:
-                field.p(i_idx, j_idx) = field.p(i_idx + 1, j_idx);
-			    field.u(i_idx, j_idx) = 0;
-			    field.v(i_idx, j_idx) = -field.v(i_idx + 1, j_idx); 
-			    field.f(i_idx, j_idx) = field.u(i_idx, j_idx);
-                break;
-            case bw:
-                field.p(i_idx, j_idx) = field.p(i_idx, j_idx - 1);
-			    field.u(i_idx, j_idx) = -field.u(i_idx, j_idx - 1);
-			    field.v(i_idx, j_idx - 1) = 0; 
-			    field.g(i_idx, j_idx - 1) = field.v(i_idx, j_idx - 1);
-                break;
-            case lw:
-                field.p(i_idx, j_idx) = field.p(i_idx - 1, j_idx);
-			    field.u(i_idx - 1, j_idx) = 0;
-			    field.v(i_idx, j_idx) = -field.v(i_idx - 1, j_idx); 
-			    field.f(i_idx - 1, j_idx) = field.u(i_idx - 1, j_idx);
-                break;
-
-        }
 		
+		if (cell.is_border(border_position::TOP) == true){
+			field.p(i_idx, j_idx) = field.p(i_idx, j_idx + 1);
+			field.u(i_idx, j_idx) = -field.u(i_idx, j_idx + 1);
+			field.v(i_idx, j_idx) = 0; 
+			field.g(i_idx, j_idx) = field.v(i_idx, j_idx);
+		}
+		else if (cell.is_border(border_position::LEFT) == true) {
+			field.p(i_idx, j_idx) = field.p(i_idx - 1, j_idx);
+			field.u(i_idx - 1, j_idx) = 0;
+			field.v(i_idx, j_idx) = -field.v(i_idx - 1, j_idx); 
+			field.f(i_idx - 1, j_idx) = field.u(i_idx - 1, j_idx);
+		}
+		else if (cell.is_border(border_position::BOTTOM) == true) {
+			field.p(i_idx, j_idx) = field.p(i_idx, j_idx - 1);
+			field.u(i_idx, j_idx) = -field.u(i_idx, j_idx - 1);
+			field.v(i_idx, j_idx - 1) = 0; 
+			field.g(i_idx, j_idx - 1) = field.v(i_idx, j_idx - 1);
+		}
+		else if (cell.is_border(border_position::RIGHT) == true) {
+			field.p(i_idx, j_idx) = field.p(i_idx + 1, j_idx);
+			field.u(i_idx, j_idx) = 0;
+			field.v(i_idx, j_idx) = -field.v(i_idx + 1, j_idx); 
+			field.f(i_idx, j_idx) = field.u(i_idx, j_idx);
+		}
 	}
-
 }
 
 MovingWallBoundary::MovingWallBoundary(std::vector<Cell *> cells, double wall_velocity) : _cells(cells) {
@@ -61,6 +53,7 @@ MovingWallBoundary::MovingWallBoundary(std::vector<Cell *> cells, std::map<int, 
                                        std::map<int, double> wall_temperature)
     : _cells(cells), _wall_velocity(wall_velocity), _wall_temperature(wall_temperature) {}
 
+//Added: Function implemented for first task.
 void MovingWallBoundary::apply(Fields &field) {
 
 	int i_idx{0};
@@ -71,39 +64,30 @@ void MovingWallBoundary::apply(Fields &field) {
 	
 		i_idx = cell->i();
 		j_idx = cell->j();
-        bool tw = cell->is_border(border_position::TOP);
-        bool rw = cell->is_border(border_position::RIGHT); 
-        bool bw = cell->is_border(border_position::BOTTOM);
-        bool lw = cell->is_border(border_position::LEFT);
-        bool con = true;
 		
-	    switch(con){
-            case tw:
-                field.p(i_idx, j_idx) = field.p(i_idx, j_idx + 1);
-			    field.u(i_idx, j_idx) = 2*vel - field.u(i_idx, j_idx + 1);
-			    field.v(i_idx, j_idx) = 0; 
-			    field.g(i_idx, j_idx) = field.v(i_idx, j_idx);
-                break;
-            case rw:
-                field.p(i_idx, j_idx) = field.p(i_idx + 1, j_idx);
-			    field.u(i_idx, j_idx) = vel;
-			    field.v(i_idx, j_idx) = -field.v(i_idx + 1, j_idx); 
-			    field.f(i_idx, j_idx) = field.u(i_idx, j_idx);
-                break;
-            case bw:
-                field.p(i_idx, j_idx) = field.p(i_idx, j_idx - 1);
-			    field.u(i_idx, j_idx) = 2*vel - field.u(i_idx, j_idx - 1);
-			    field.v(i_idx, j_idx - 1) = 0; 
-			    field.g(i_idx, j_idx - 1) = field.v(i_idx, j_idx - 1);
-                break;
-            case lw:
-                field.p(i_idx, j_idx) = field.p(i_idx - 1, j_idx);
-			    field.u(i_idx - 1, j_idx) = vel;
-			    field.v(i_idx, j_idx) = -field.v(i_idx - 1, j_idx); 
-			    field.f(i_idx - 1, j_idx) = field.u(i_idx - 1, j_idx);
-                break;
-
-        }
+		if (cell.is_border(border_position::TOP) == true) {
+			field.p(i_idx, j_idx) = field.p(i_idx, j_idx + 1);
+			field.u(i_idx, j_idx) = 2*vel - field.u(i_idx, j_idx + 1);
+			field.v(i_idx, j_idx) = 0; 
+			field.g(i_idx, j_idx) = field.v(i_idx, j_idx);
+		}
+		else if (cell.is_border(border_position::LEFT) == true) {
+			field.p(i_idx, j_idx) = field.p(i_idx - 1, j_idx);
+			field.u(i_idx - 1, j_idx) = vel;
+			field.v(i_idx, j_idx) = -field.v(i_idx - 1, j_idx); 
+			field.f(i_idx - 1, j_idx) = field.u(i_idx - 1, j_idx);
+		}
+		else if (cell.is_border(border_position::BOTTOM) == true) {
+			field.p(i_idx, j_idx) = field.p(i_idx, j_idx - 1);
+			field.u(i_idx, j_idx) = 2*vel - field.u(i_idx, j_idx - 1);
+			field.v(i_idx, j_idx - 1) = 0; 
+			field.g(i_idx, j_idx - 1) = field.v(i_idx, j_idx - 1);
+		}
+		else if (cell.is_border(border_position::RIGHT) == true) {
+			field.p(i_idx, j_idx) = field.p(i_idx + 1, j_idx);
+			field.u(i_idx, j_idx) = vel;
+			field.v(i_idx, j_idx) = -field.v(i_idx + 1, j_idx); 
+			field.f(i_idx, j_idx) = field.u(i_idx, j_idx);
+		}
 	}
-
 }
