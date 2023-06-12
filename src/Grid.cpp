cdfc8a0b9cb5a275b6d02ef99d8b3cf0bab16bd0
Felipe Díaz Laverde <fa.diaz@tum.de>
2023-06-12 22:33:49 +0200

    Upload New File

---
+++ src/Grid.cpp
@@ -0,0 +1,379 @@
+#include "Grid.hpp"
+#include "Enums.hpp"
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+Grid::Grid(std::string geom_name, Domain &domain, int _iproc, int _jproc, int imax, int jmax ) {
+
+    int size, my_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    _domain = domain;
+    int *buffer[domain.jmax] = {0};
+
+    _cells = Matrix<Cell>(_domain.size_x + 2, _domain.size_y + 2);
+
+    if (!geom_name.compare("NONE")) {
+        std::vector<std::vector<int>> geometry_data(_domain.domain_size_x + 2,
+                                                    std::vector<int>(_domain.domain_size_y + 2, 0));
+        
+        if (my_rank == 0){
+        std::vector<std::vector<int>> geometry_master(imax+ 2,
+                                                    std::vector<int>(jmax + 2, 0));
+        	parse_geometry_file(geom_name, geometry_data);
+        	for (int i = _domain.imin - 1; i < _domain.imax + 1; ++i){
+			for(int j = _domain.jmin - 1; j < _domain.jmax + 1; ++j){
+				geometry_data[i][j] = geometry_master[i][j];
+			}
+        		 
+		} 
+		
+        	for(int k = 1; k < size; ++k){
+        	
+			int imin_local;
+			int imax_local;
+			int jmin_local;
+			int jmax_local;
+			
+			int my_x = (k)%_iproc + 1;
+			int my_y = ((k + 1) - my_x)/_iproc + 1;
+			if ((imax % _iproc) == 0){ //All processors take same number of tiles (x-axis).
+				imin_local = (my_x - 1)*(imax/_iproc) + 1;
+				imax_local = (my_x)*(imax/_iproc);
+				
+				if ((jmax % _jproc) == 0){ //All processors take same number of tiles (y-axis).
+					jmin_local = (my_y - 1)*(jmax/_jproc) + 1;
+					jmax_local = (my_y)*(jmax/_jproc);
+				}
+				else{ //Not all processors take same number of tiles (y-axis).
+				    	if (my_y <= (jmax % _jproc)){ //Processors with an extra tile (y-axis).
+				    		jmin_local = (my_y - 1)*(jmax/_jproc) + (my_y);
+						jmax_local = (my_y)*(jmax/_jproc) + (my_y);
+				 	}
+				 	else{ //Processors with no extra tile (y-axis).
+				 		jmin_local = (my_y - 1)*(jmax/_jproc) + (jmax % _jproc + 1);
+						jmax_local = (my_y)*(jmax/_jproc) + (jmax % _jproc);
+				 	}
+				}
+				
+				    
+		    }
+		    
+		    else{ //Not all processors take same number of tiles (x-axis).
+		    	if (my_x <= (imax % _iproc)){ //Processors with an extra tile (x-axis).
+		    		imin_local = (my_x - 1)*(imax/_iproc) + (my_x);
+				imax_local = (my_x)*(imax/_iproc) + (my_x);
+		 	}
+		 	else{ //Processors with no extra tile (x-axis).
+		 		imin_local = (my_x - 1)*(imax/_iproc) + (imax % _iproc + 1);
+				imax_local = (my_x)*(imax/_iproc) + (imax % _iproc);
+		 	}
+		 	if ((jmax % _jproc) == 0){ //All processors take same number of tiles (y-axis).
+				jmin_local = (my_y - 1)*(jmax/_jproc) + 1;
+				jmax_local = (my_y)*(jmax/_jproc);
+			}
+			else{ //Not all processors take same number of tiles (y-axis).
+			    	if (my_y <= (jmax % _jproc)){ //Processors with an extra tile (y-axis).
+			    		jmin_local = (my_y - 1)*(jmax/_jproc) + (my_y);
+					jmax_local = (my_y)*(jmax/_jproc) + (my_y);
+			 	}
+			 	else{ //Processors with no extra tile (y-axis).
+			 		jmin_local = (my_y - 1)*(jmax/_jproc) + (jmax % _jproc + 1);
+					jmax_local = (my_y)*(jmax/_jproc) + (jmax % _jproc);
+			 	}
+			}
+	    	    }
+		   
+		for (int i = imin_local - 1; i < imax_local + 1; ++i){
+		        int s{0};
+			for(int j = jmin_local - 1; j < jmax_local + 1; ++j){
+				
+				*buffer[s] = geometry_data[i][j];
+				s++;
+			}
+        		MPI_Send(*buffer, s, MPI_INT, k, 1, MPI_COMM_WORLD);
+        		 
+		} 
+        	
+        	}
+            }
+            else{
+            	MPI_Recv(*buffer, domain.jmax, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            	for (int j = _domain.jmin - 1; j < _domain.jmax + 1; ++j){
+            		for (int i = _domain.imin - 1; i < domain.imax + 1; ++i){
+            			int s = 0;
+            			geometry_data[i][j] = *buffer[s];
+            			s++;
+            		}
+            	}
+            }
+        assign_cell_types(geometry_data);
+        } else {
+            build_lid_driven_cavity();
+        }
+}
+
+
+void Grid::build_lid_driven_cavity() {
+    std::vector<std::vector<int>> geometry_data(_domain.domain_size_x + 2,
+                                                std::vector<int>(_domain.domain_size_y + 2, 0));
+
+    for (int i = 0; i < _domain.domain_size_x + 2; ++i) {
+        for (int j = 0; j < _domain.domain_size_y + 2; ++j) {
+            // Bottom, left and right walls: no-slip
+            if (i == 0 || j == 0 || i == _domain.domain_size_x + 1) {
+                geometry_data.at(i).at(j) = LidDrivenCavity::fixed_wall_id;
+            }
+            // Top wall: moving wall
+            else if (j == _domain.domain_size_y + 1) {
+                geometry_data.at(i).at(j) = LidDrivenCavity::moving_wall_id;
+            }
+        }
+    }
+    assign_cell_types(geometry_data);
+}
+
+void Grid::assign_cell_types(std::vector<std::vector<int>> &geometry_data) {
+
+    int i = 0;
+    int j = 0;
+
+    for (int j_geom = _domain.jmin; j_geom < _domain.jmax; ++j_geom) {
+        { i = 0; }
+        for (int i_geom = _domain.imin; i_geom < _domain.imax; ++i_geom) {
+            if (geometry_data.at(i_geom).at(j_geom) == 0) {
+            	if( (i == 0) || (j == 0) || (i == _domain.imax - 1) || (j == _domain.jmax - 1)){
+            		_cells(i, j) = Cell(i, j, cell_type::FLUID, true);
+            	}
+            	else{
+                	_cells(i, j) = Cell(i, j, cell_type::FLUID);
+                	_fluid_cells.push_back(&_cells(i, j));
+            	}
+            } 
+            else if (geometry_data.at(i_geom).at(j_geom) == LidDrivenCavity::moving_wall_id) {
+                _cells(i, j) = Cell(i, j, cell_type::MOVING_WALL, geometry_data.at(i_geom).at(j_geom));
+                _moving_wall_cells.push_back(&_cells(i, j));
+               
+            } 
+            else if (geometry_data.at(i_geom).at(j_geom) == 1) {
+                _cells(i, j) = Cell(i, j, cell_type::INFLOW, geometry_data.at(i_geom).at(j_geom));
+                _inflow_cells.push_back(&_cells(i, j));
+            }
+            else if (geometry_data.at(i_geom).at(j_geom) == 2) {
+                _cells(i, j) = Cell(i, j, cell_type::OUTFLOW, geometry_data.at(i_geom).at(j_geom));
+                _outflow_cells.push_back(&_cells(i, j));
+            }
+            else {
+                // Outer walls and inner obstacles
+                _cells(i, j) = Cell(i, j, cell_type::FIXED_WALL, geometry_data.at(i_geom).at(j_geom));
+                _fixed_wall_cells.push_back(&_cells(i, j));
+            }
+
+            ++i;
+        }
+        ++j;
+    }
+
+    // Corner cell neighbour assigment
+    // Bottom-Left Corner
+    i = 0;
+    j = 0;
+    _cells(i, j).set_neighbour(&_cells(i, j + 1), border_position::TOP);
+    _cells(i, j).set_neighbour(&_cells(i + 1, j), border_position::RIGHT);
+    if (_cells(i, j).neighbour(border_position::TOP)->type() == cell_type::FLUID) {
+        _cells(i, j).add_border(border_position::TOP);
+    }
+    if (_cells(i, j).neighbour(border_position::RIGHT)->type() == cell_type::FLUID) {
+        _cells(i, j).add_border(border_position::RIGHT);
+    }
+    // Top-Left Corner
+    i = 0;
+    j = _domain.size_y + 1;
+    _cells(i, j).set_neighbour(&_cells(i, j - 1), border_position::BOTTOM);
+    _cells(i, j).set_neighbour(&_cells(i + 1, j), border_position::RIGHT);
+    if (_cells(i, j).neighbour(border_position::BOTTOM)->type() == cell_type::FLUID) {
+        _cells(i, j).add_border(border_position::BOTTOM);
+    }
+    if (_cells(i, j).neighbour(border_position::RIGHT)->type() == cell_type::FLUID) {
+        _cells(i, j).add_border(border_position::RIGHT);
+    }
+
+    // Top-Right Corner
+    i = _domain.size_x + 1;
+    j = Grid::_domain.size_y + 1;
+    _cells(i, j).set_neighbour(&_cells(i, j - 1), border_position::BOTTOM);
+    _cells(i, j).set_neighbour(&_cells(i - 1, j), border_position::LEFT);
+    if (_cells(i, j).neighbour(border_position::BOTTOM)->type() == cell_type::FLUID) {
+        _cells(i, j).add_border(border_position::BOTTOM);
+    }
+    if (_cells(i, j).neighbour(border_position::LEFT)->type() == cell_type::FLUID) {
+        _cells(i, j).add_border(border_position::LEFT);
+    }
+
+    // Bottom-Right Corner
+    i = Grid::_domain.size_x + 1;
+    j = 0;
+    _cells(i, j).set_neighbour(&_cells(i, j + 1), border_position::TOP);
+    _cells(i, j).set_neighbour(&_cells(i - 1, j), border_position::LEFT);
+    if (_cells(i, j).neighbour(border_position::TOP)->type() == cell_type::FLUID) {
+        _cells(i, j).add_border(border_position::TOP);
+    }
+    if (_cells(i, j).neighbour(border_position::LEFT)->type() == cell_type::FLUID) {
+        _cells(i, j).add_border(border_position::LEFT);
+    }
+    // Bottom cells
+    j = 0;
+    for (int i = 1; i < _domain.size_x + 1; ++i) {
+        _cells(i, j).set_neighbour(&_cells(i + 1, j), border_position::RIGHT);
+        _cells(i, j).set_neighbour(&_cells(i - 1, j), border_position::LEFT);
+        _cells(i, j).set_neighbour(&_cells(i, j + 1), border_position::TOP);
+        if (_cells(i, j).neighbour(border_position::RIGHT)->type() == cell_type::FLUID) {
+            _cells(i, j).add_border(border_position::RIGHT);
+        }
+        if (_cells(i, j).neighbour(border_position::LEFT)->type() == cell_type::FLUID) {
+            _cells(i, j).add_border(border_position::LEFT);
+        }
+        if (_cells(i, j).neighbour(border_position::TOP)->type() == cell_type::FLUID) {
+            _cells(i, j).add_border(border_position::TOP);
+        }
+    }
+
+    // Top Cells
+    j = Grid::_domain.size_y + 1;
+
+    for (int i = 1; i < _domain.size_x + 1; ++i) {
+        _cells(i, j).set_neighbour(&_cells(i + 1, j), border_position::RIGHT);
+        _cells(i, j).set_neighbour(&_cells(i - 1, j), border_position::LEFT);
+        _cells(i, j).set_neighbour(&_cells(i, j - 1), border_position::BOTTOM);
+        if (_cells(i, j).neighbour(border_position::RIGHT)->type() == cell_type::FLUID) {
+            _cells(i, j).add_border(border_position::RIGHT);
+        }
+        if (_cells(i, j).neighbour(border_position::LEFT)->type() == cell_type::FLUID) {
+            _cells(i, j).add_border(border_position::LEFT);
+        }
+        if (_cells(i, j).neighbour(border_position::BOTTOM)->type() == cell_type::FLUID) {
+            _cells(i, j).add_border(border_position::BOTTOM);
+        }
+    }
+
+    // Left Cells
+    i = 0;
+    for (int j = 1; j < _domain.size_y + 1; ++j) {
+        _cells(i, j).set_neighbour(&_cells(i + 1, j), border_position::RIGHT);
+        _cells(i, j).set_neighbour(&_cells(i, j - 1), border_position::BOTTOM);
+        _cells(i, j).set_neighbour(&_cells(i, j + 1), border_position::TOP);
+        if (_cells(i, j).neighbour(border_position::RIGHT)->type() == cell_type::FLUID) {
+            _cells(i, j).add_border(border_position::RIGHT);
+        }
+        if (_cells(i, j).neighbour(border_position::BOTTOM)->type() == cell_type::FLUID) {
+            _cells(i, j).add_border(border_position::BOTTOM);
+        }
+        if (_cells(i, j).neighbour(border_position::TOP)->type() == cell_type::FLUID) {
+            _cells(i, j).add_border(border_position::TOP);
+        }
+    }
+    // Right Cells
+    i = Grid::_domain.size_x + 1;
+    for (int j = 1; j < _domain.size_y + 1; ++j) {
+        _cells(i, j).set_neighbour(&_cells(i - 1, j), border_position::LEFT);
+        _cells(i, j).set_neighbour(&_cells(i, j - 1), border_position::BOTTOM);
+        _cells(i, j).set_neighbour(&_cells(i, j + 1), border_position::TOP);
+        if (_cells(i, j).neighbour(border_position::LEFT)->type() == cell_type::FLUID) {
+            _cells(i, j).add_border(border_position::LEFT);
+        }
+        if (_cells(i, j).neighbour(border_position::BOTTOM)->type() == cell_type::FLUID) {
+            _cells(i, j).add_border(border_position::BOTTOM);
+        }
+        if (_cells(i, j).neighbour(border_position::TOP)->type() == cell_type::FLUID) {
+            _cells(i, j).add_border(border_position::TOP);
+        }
+    }
+
+    // Inner cells
+    for (int i = 1; i < _domain.size_x + 1; ++i) {
+        for (int j = 1; j < _domain.size_y + 1; ++j) {
+            _cells(i, j).set_neighbour(&_cells(i + 1, j), border_position::RIGHT);
+            _cells(i, j).set_neighbour(&_cells(i - 1, j), border_position::LEFT);
+            _cells(i, j).set_neighbour(&_cells(i, j + 1), border_position::TOP);
+            _cells(i, j).set_neighbour(&_cells(i, j - 1), border_position::BOTTOM);
+
+            if (_cells(i, j).type() != cell_type::FLUID) {
+                if (_cells(i, j).neighbour(border_position::LEFT)->type() == cell_type::FLUID) {
+                    _cells(i, j).add_border(border_position::LEFT);
+                }
+                if (_cells(i, j).neighbour(border_position::RIGHT)->type() == cell_type::FLUID) {
+                    _cells(i, j).add_border(border_position::RIGHT);
+                }
+                if (_cells(i, j).neighbour(border_position::BOTTOM)->type() == cell_type::FLUID) {
+                    _cells(i, j).add_border(border_position::BOTTOM);
+                }
+                if (_cells(i, j).neighbour(border_position::TOP)->type() == cell_type::FLUID) {
+                    _cells(i, j).add_border(border_position::TOP);
+                }
+            }
+        }
+    }
+}
+
+void Grid::parse_geometry_file(std::string filedoc, std::vector<std::vector<int>> &geometry_data) {
+
+    int num_cells_in_x, num_cells_in_y, depth;
+
+    std::ifstream infile(filedoc);
+    std::stringstream ss;
+    std::string inputLine = "";
+
+    // First line : version
+    getline(infile, inputLine);
+    if (inputLine.compare("P2") != 0) {
+        std::cerr << "First line of the PGM file should be P2" << std::endl;
+    }
+
+    // Second line : comment
+    getline(infile, inputLine);
+
+    // Continue with a stringstream
+    ss << infile.rdbuf();
+    // Third line : size
+    ss >> num_cells_in_x >> num_cells_in_y;
+    // Fourth line : depth
+    ss >> depth;
+
+    // Following lines : data (origin of x-y coordinate system in bottom-left corner)
+    for (int y = num_cells_in_y - 1; y > -1; --y) {
+        for (int x = 0; x < num_cells_in_x; ++x) {
+            ss >> geometry_data[x][y];
+        }
+    }
+
+    infile.close();
+}
+
+int Grid::imax() const { return _domain.size_x; }
+int Grid::jmax() const { return _domain.size_y; }
+
+int Grid::imaxb() const { return _domain.size_x + 2; }
+int Grid::jmaxb() const { return _domain.size_y + 2; }
+
+Cell Grid::cell(int i, int j) const { return _cells(i, j); }
+
+double Grid::dx() const { return _domain.dx; }
+
+double Grid::dy() const { return _domain.dy; }
+
+const Domain &Grid::domain() const { return _domain; }
+
+const std::vector<Cell *> &Grid::fluid_cells() const { return _fluid_cells; }
+
+const std::vector<Cell *> &Grid::fixed_wall_cells() const { return _fixed_wall_cells; }
+
+const std::vector<Cell *> &Grid::moving_wall_cells() const { return _moving_wall_cells; }
+
+const std::vector<Cell *> &Grid::inflow_cells() const { return _inflow_cells; }
+
+const std::vector<Cell *> &Grid::outflow_cells() const { return _outflow_cells; }
+
