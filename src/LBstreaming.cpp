ff58850acd2909cba24bdd645b881345e5a1394a
Siva Karthikeya <writetokarthikeya99@gmail.com>
2023-06-30 10:53:31 +0200

    Comments

--- src/LBstreaming.cpp
+++ src/LBstreaming.cpp
@@ -5,7 +5,7 @@
 void doStreaming(double *collideField, double *streamField,int *flagField,int xlength){
     int dx, dy;
     double fi;
-    // setting distribution function for each moving direction/lattice velocity of every particle
+    
     for (int y = 1; y < xlength + 1; ++y) {
         for (int x = 1; x < xlength + 1; ++x) {
             for (int i = 0; i < Q; ++i) {
@@ -13,12 +13,7 @@ void doStreaming(double *collideField, double *streamField,int *flagField,int xl
                 // dx = c_i_x*dt, where dt = 1, etc.
                 dx = LATTICEVELOCITIES[i][0];
                 dy = LATTICEVELOCITIES[i][1];
-                /*New value for our distribution function (DF) of the index 'i'
-
-                    (We set it to DF(i) of the next particle, whose i-th lattice velocity
-                    points towards considered particle (x,y))
-
-                    Position of that next particle is given by (x-dx, y-dy)*/
+                
 
                 fi = collideField [ Q * ((y-dy)*(xlength+2) + x - dx) + i ];
                 streamField [Q * (y*(xlength+2) + x) + i ] = fi;
